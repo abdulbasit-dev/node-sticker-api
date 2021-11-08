f45ab1ffde5c303efda3595ab1ef04f5d04384c2
abdulbasit-dev <abdullbasit99salah@gmail.com>
2021-11-08 11:11:44 +0300

    remove database name

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
       port:process.env.DB_PORT,
       user: 'root',
       password: process.env.DB_PASSWORD,
-      database:'node_knex-starter',
+      database:'',
     },
     pool: {
       min: 0,
